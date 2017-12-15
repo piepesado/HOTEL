29618614feb0f30f30e04d7780c6c41703d4574a
Diego Nan <dnan@integraccs.com>
2017-12-15 10:32:37 -0300

    Add project files.

---
+++ AirDemo.Specs/Pages/HotelSearchPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirDemo.Specs.Pages
+{
+    public class HotelSearchPage : BasePage
+    {
+        [FindsBy(How = How.LinkText, Using = "Flight")]
+        private IWebElement _flightLink;
+
+        public HotelSearchPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void EnsurePageIsLoaded()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementToBeClickable(_flightLink));
+        }
+
+        /*
+        public FlightSearchPage ClickFlightLink()
+        {
+            _flightLink.Click();
+            return new FlightSearchPage(_driver);
+        }
+        */
+    }
+}
